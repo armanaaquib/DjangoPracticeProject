b9cf506dce7db152e8587ce2c4d500df0b3f1572
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-03-22 08:02:27 +0530

    script to populate users

---
+++ populate_users.py
@@ -0,0 +1,24 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','user_list.settings')
+
+import django
+django.setup()
+
+from users.models import Users
+
+from faker import Faker
+fakegen = Faker()
+
+def populate(N=5):
+  
+  for entry in range(N):
+    
+    fake_first_name = fakegen.first_name()
+    fake_last_name = fakegen.last_name()
+    fake_email = fakegen.email()
+
+    Users.objects.create(first_name=fake_first_name,last_name=fake_last_name,email=fake_email)
+
+if __name__ == '__main__':
+  print('populating...')
+  populate(20)
