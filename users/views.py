51d282fb911b4770b768d146ceddb1740f06dc14
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-03-22 08:34:56 +0530

    added users views

--- users/views.py
+++ users/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from users.models import Users
 
 # Create your views here.
 def index(request):
   return render(request,'index.html')
+
+def users(request):
+  users = Users.objects.all()
+  return render(request, 'users/users.html', {'users':users})
