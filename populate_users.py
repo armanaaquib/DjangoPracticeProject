e21e16a6bfd1780f8c34573e3721238a040cd73f
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-03-22 09:18:15 +0530

    changed model Users to User

--- populate_users.py
+++ populate_users.py
@@ -4,7 +4,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE','user_list.settings')
 import django
 django.setup()
 
-from users.models import Users
+from users.models import User
 
 from faker import Faker
 fakegen = Faker()
@@ -17,7 +17,7 @@ def populate(N=5):
     fake_last_name = fakegen.last_name()
     fake_email = fakegen.email()
 
-    Users.objects.create(first_name=fake_first_name,last_name=fake_last_name,email=fake_email)
+    User.objects.create(first_name=fake_first_name,last_name=fake_last_name,email=fake_email)
 
 if __name__ == '__main__':
   print('populating...')
--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from users.models import Users
+from users.models import User
 
 # Create your views here.
 def index(request):
   return render(request,'index.html')
 
 def users(request):
-  users = Users.objects.all()
+  users = User.objects.all()
   return render(request, 'users/users.html', {'users':users})
