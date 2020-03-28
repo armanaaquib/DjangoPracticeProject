9e71d19c0bfde70d595693c6b502f137c27fc3c9
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-03-28 08:29:46 +0530

    add user view

--- users/views.py
+++ users/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-from users.models import User
+from . import models, forms
 
 # Create your views here.
 def index(request):
   return render(request,'index.html')
 
 def users(request):
-  users = User.objects.all()
+  users = models.User.objects.all()
   return render(request, 'users/users.html', {'users':users})
+
+def addUser(request):
+
+  if request.method == 'POST':
+    userForm = forms.UserForm(request.POST)
+
+    if userForm.is_valid:
+      userForm.save()
+      return users(request)
+
+  userForm = forms.UserForm()
+  return render(request, 'users/addUser.html',{'form' : userForm})
