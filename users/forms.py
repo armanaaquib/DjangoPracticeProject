ac0f9fe739bfdb2bd445ac1a43cbaef45a2fbc2f
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-04-06 15:23:41 +0530

    added confirm password

--- users/forms.py
+++ users/forms.py
@@ -11,10 +11,19 @@ class UserForm(forms.ModelForm):
 
 class UserInfoForm(forms.ModelForm):
   password = forms.CharField(widget=forms.PasswordInput(), validators=[validate_password])
+  confirm_password = forms.CharField(widget=forms.PasswordInput())
   
   class Meta:
     model = User
-    fields = ('username', 'email', 'password')
+    fields = ('username', 'email', 'password', 'confirm_password')
+  
+  def clean(self):
+    cleaned_data = super().clean()
+    password = cleaned_data.get('password')
+    confirm_password = cleaned_data.get('confirm_password')
+
+    if password != confirm_password:
+      raise forms.ValidationError('Passwords are not equal.')
 
 class UserProfileInfoForm(forms.ModelForm):
 
