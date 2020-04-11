795243b5b6c5cc8bec7bd78bb584f32e2a289551
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-04-11 10:12:43 +0530

    added: TestSchoolModel

--- school/tests.py
+++ school/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import School, Student
 
 # Create your tests here.
+class TestSchoolModel(TestCase):
+  def test_get_absolute_url(self):
+    school = School(pk='1')
+    self.assertEqual(school.get_absolute_url(), '/schools/1/')
+
+  def test_str(self):
+    school = School(name='MANUU')
+    self.assertEqual(school.__str__(), 'MANUU')
